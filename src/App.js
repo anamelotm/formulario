1a4c037b1f0196d81a90ece254bf5b31c97757d2
anamelotm <analuisa.tomita@hotmail.com>
2021-07-15 17:42:33 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,69 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class Formulario extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {nome: '', linguagem: 'Javascript', tipo: 'programador', 
+dedico: true, bio: ''}
+
+    this.handleSubmit = (event) => {
+      event.preventDefault();
+      console.log(this.state);
+    }
+
+    this.changeName = (event) => {
+      this.setState({nome: event.target.value});
+    }
+
+    this.changeSelect = (event) => {
+      this.setState({linguagem: event.target.value});
+    }
+
+    this.changeRadio = (event) => {
+      this.setState({tipo: event.target.value});
+    }
+
+    this.changeCheckbox = (event) => {
+      this.setState({dedico: event.target.checked});
+    }
+
+    this.changeBio = (event) => {
+      this.setState({bio: event.target.value});
+    }
+  }
+
+  render() {
+    return (
+      <>
+      <form onSubmit={this.handleSubmit}>
+        <label>
+          Nome: <input type="text" value={this.state.nome} onChange={this.changeName}></input><br/> 
+        </label>
+        <label>
+          Linguagem Favorita: 
+          <select value={this.state.linguagem} onChange={this.changeSelect}>
+            <option>Javascript</option>
+            <option>Python</option>
+            <option>C++</option>
+          </select>
+        </label> <br/>
+        <label>
+          Sou: 
+          <input type="radio" checked={this.state.tipo == 'programador'} onChange={this.changeRadio} value='programador'></input> Programador
+          <input type="radio" checked={this.state.tipo == 'estudante'} onChange={this.changeRadio} value='estudante'></input>Estudante
+        </label> <br/>
+        <label>
+          <input type="checkbox" checked={this.state.dedico} onChange={this.changeCheckbox}></input>Dedico 8h semanais aos estudos.
+        </label> <br/>
+        <label>
+          Bio: 
+          <textarea cols='50' value={this.state.bio} onChange={this.changeBio} />
+        </label> <br/>
+        <input type="submit" value='Salvar'/>
+      </form>
+      </>
+    );
+  }
 }
 
-export default App;
+export default Formulario;
